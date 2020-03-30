01e9ebe03bfac2cbdd887debe75a1066cb4a7fbf
iwiniwin <iwiniwin@foxmail.com>
2020-03-30 23:20:00 +0800

    实现怪物AI

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Wander))]
+public class Enemy : MonoBehaviour
+{
+
+    [Tooltip("障碍物检测点")]
+    [SerializeField]
+    private Transform m_FrontCheck;
+
+    private Wander m_Wander;
+    private LayerMask m_LayerMask;
+
+    private void Awake() {
+        m_Wander = GetComponent<Wander>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_LayerMask = LayerMask.GetMask("Obstacle");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D[] frontHits = Physics2D.OverlapPointAll(m_FrontCheck.position, m_LayerMask);
+        if(frontHits.Length > 0){
+            m_Wander.Flip();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Wander.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+
+    [Tooltip("是否朝向右边")]
+    [SerializeField]
+    private bool m_FacingRight = true;
+
+    [Tooltip("移动速度")]
+    [SerializeField]
+    private float m_MoveSpeed = 2f;
+
+    private Rigidbody2D m_Rigidbody;
+
+    private float m_CurrentMoveSpeed;
+
+    private void Awake() {
+        m_Rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(m_FacingRight){
+            m_CurrentMoveSpeed = m_MoveSpeed;
+        }else{
+            m_CurrentMoveSpeed = -m_MoveSpeed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        m_Rigidbody.velocity = new Vector2(m_CurrentMoveSpeed, m_Rigidbody.velocity.y);
+    }
+
+    public void Flip(){
+        m_CurrentMoveSpeed = -m_CurrentMoveSpeed;
+        // Vector3.Scale 将两个向量按分量相乘
+        transform.localScale = Vector3.Scale(new Vector3(-1, 1, 1), transform.localScale);
+    }
+}
