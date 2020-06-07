6274517d7cfe78dba54540c045fb9900f34c00f0
iwiniwin <iwiniwin@foxmail.com>
2020-06-07 18:42:32 +0800

    实现角色和怪物落入水中并销毁

---
+++ Assets/Scripts/Utility/Remover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Remover : MonoBehaviour
+{
+    [Tooltip("浪花预设")]
+    public GameObject SplashPrefab;
+
+    private BoxCollider2D m_Trigger;
+
+    private void Awake() {
+        m_Trigger = GetComponent<BoxCollider2D>();
+        m_Trigger.isTrigger = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        Instantiate(SplashPrefab, collision.transform.position, transform.rotation);
+        Destroy(collision.gameObject);
+    }
+}
